6a469fbd3b9d3c310b0d32722fa195021bcfa662
Akshay kale <56558832+ak-akshaykale@users.noreply.github.com>
2020-01-01 18:09:37 +0530

    Add files via upload

---
+++ DeligatesEvents/MulThreading/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MulThreading
+{
+    class Program
+    {
+        //calling method by anonymous method & BeginInvoke
+        static void Main1(string[] args)
+        {
+
+            Func<int, int> add = (i) => i + i;
+            add.BeginInvoke(10, delegate (IAsyncResult rs) {
+                int result = add.EndInvoke(rs);
+                Console.WriteLine(" :: " + result);
+            }, add);
+            Console.ReadLine();
+        }
+
+        class Para
+        {
+            public int i;
+            public int j;
+        }
+
+        // objThread.Start();
+        static void Main2()
+        {
+
+            //Func<Para, int> mulc = (t) => t.i = t.i + t.j;
+            Thread mulThread = new Thread(new ThreadStart(delegate {
+                Console.WriteLine("Thread is running with anonymous function");
+            }));
+            mulThread.Start();
+            Console.ReadLine();
+        }
+        static void Main3()
+        {
+
+            Action action = delegate {
+                Console.WriteLine("Hello!");
+            };
+            Action<int> ij =myThread1;
+            ij(10);
+            Thread actionThread = new Thread(new ThreadStart(action));
+            actionThread.Start();
+            Console.ReadLine();
+        }
+
+        static void Main()
+        {
+            //Thread thread = new Thread(new ThreadStart(my))
+            //delegate 
+            ThreadPool.QueueUserWorkItem(new WaitCallback(myThread3),5);
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback(dee), 6);
+            // ThreadPool.
+            int workerThreads;
+            int completeCode;
+            ThreadPool.GetMaxThreads(out workerThreads,out completeCode);
+            Console.WriteLine(" "+workerThreads+" "+completeCode);
+            Console.ReadLine();
+        }
+        static void myThread3(object a)
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                Console.WriteLine("thead1 runnnung in thread pool" + a);
+            }
+            
+            //return 00;
+        }
+
+        static void dee(object ii)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+
+                Console.WriteLine("ok ok " + ii);
+            }
+        }
+
+
+        static void myThread1(int a)
+        {
+            Console.WriteLine("thead1 runnnung in thread pool");
+           // return 00;
+        }
+        static void myThread2()
+        {
+            Console.WriteLine("thread2 runnnung in thread pool");
+        }
+    }
+}
---
+++ DeligatesEvents/lambdas/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lambdas
+{
+    class Program
+    {
+        class Employee
+        {
+            public decimal Basic { set; get; }
+            
+            //publi
+        }
+        static void Main()
+        {
+            //Lambda function 1
+            Func<int, int> getDouble = i => i*2;
+            /* getDouble.BeginInvoke(80, delegate (IAsyncResult rs) {
+                 int result = getDouble.EndInvoke(rs);
+                 Console.WriteLine(result);
+             },getDouble);*/
+
+            //Interlocked.
+            
+            Predicate<Employee> preEmp = (emp) => emp.Basic > 10000;
+
+            Console.WriteLine("Employee:: "+    preEmp(new Employee { Basic=50000 })); 
+            //Console.WriteLine(asd);
+            Func<int, bool> isEven = i => i%2==0;
+            isEven.BeginInvoke(21, delegate(IAsyncResult ar) {
+                bool result = isEven.EndInvoke(ar);
+                Console.WriteLine("isEven {0}",result);
+            }, null);
+
+            //default only boolean as return datatype
+            Predicate<int> isGreater = (i) => i>80;
+            
+            //#1
+            /*isGreater.BeginInvoke(100,delegate(IAsyncResult rs){
+                bool br=isGreater.EndInvoke(rs);
+                Console.WriteLine(br);
+            },null);
+            */
+            //#2
+            Console.WriteLine(isGreater(110));
+
+            //always returns void
+            Action<int> a = helloRag;
+            a(0);
+
+            //only in para , out is void always
+            Action<int, object> objAction = helloObject;
+            objAction(12, new object());
+
+
+            //in para , out para
+            Func<int,int> objFunc = sayFunc;
+            objFunc(11);
+
+            Console.ReadLine();
+        }
+        static void helloRag(int a)
+        {
+            Console.WriteLine("Hello raga is of "+a);
+        }
+        static void helloObject(int a, object b)
+        {
+            Console.WriteLine(b+" of value is "+a);
+        }
+
+        static int sayFunc(int x)
+        {
+            Console.WriteLine("say hello by func delegate which have return type "+x);
+            return 00;
+        }
+    }
+}
