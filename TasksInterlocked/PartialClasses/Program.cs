f5f1a18ca6551a24741672b203746c4245ed4da4
Akshay kale <56558832+ak-akshaykale@users.noreply.github.com>
2020-01-01 17:07:06 +0530

    Add files via upload

---
+++ TasksInterlocked/PartialClasses/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClasses
+{
+    partial class Myclass
+    {
+        int a;
+        int b;
+
+        //partial method return type is void only
+        partial void dislpay(int a);
+
+        public int change()
+        {
+            dislpay(10);
+            return 25;
+        }
+
+    }
+
+    partial class Myclass
+    {
+        partial void dislpay(int a)
+        {
+            this.a = a;
+            b = a * a;
+            Console.WriteLine("a::{0} b:{1}",a,b);
+        }
+    }
+    class Program
+    {
+      //  class StringMethodAdder
+        //{
+            //shloud be static with the parameter this class(that class that we want to extension metnod)
+            static string StringWordReverser(this string ss)
+            {
+                return ss + (0 * 0 * 0) + ss;
+            }
+      //  }
+        static void Main(string[] args)
+        {
+            Myclass m = new Myclass();
+            Console.WriteLine(  m.change());
+
+            //ExtensionMethod.StringMethodAdder str = new ExtensionMethod.StringMethodAdder();
+            //str.
+            string str = "asdds";
+            //str.
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ TasksInterlocked/TasksInterlocked/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace TasksInterlocked
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // var a = new AppDomainSetup;
+
+            //Anonymous class
+            var a = new {NAME="adsd",OO="adsad" };
+
+            //same class in generated in back
+            var b = new {NAME="adsd",OO="adsad" };
+
+            ///de
+            var c = new { NAME = "adsd", OO = "adsad",UU=465 };
+
+
+            int i = 10;
+            double d = 0;
+            double d2 = 0;
+            //i = i + 12;
+            int isa =Interlocked.Add(ref i, 12);
+            double ss= Interlocked.CompareExchange(ref d2, d2,11.11);
+            Console.WriteLine(isa);
+            Console.WriteLine(ss);
+
+
+            //#1
+            Task t = new Task(hello);
+            t.Start();
+            //#2
+            Action ae = hello;
+            Task.Run(ae);
+
+            //#3
+            Task.Factory.StartNew(ae);
+
+            Action displayAction = display;
+            Task task1 = new Task(displayAction);
+            task1.Start();
+            Console.ReadLine();
+        }
+        static void hello()
+        {
+            Thread.Sleep(8000);
+            Console.WriteLine("Welcome to JUHU");
+
+        }
+
+        static void display()
+        {
+            int i = 0;
+            while (i < 4654646)
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("DIS :: ");
+            }
+        }
+            //return data;
+       
+    }
+}
