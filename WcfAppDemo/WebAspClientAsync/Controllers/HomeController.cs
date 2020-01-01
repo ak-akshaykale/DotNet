4036aab436af237b8433b423bf46f66a6909cd79
Akshay kale <56558832+ak-akshaykale@users.noreply.github.com>
2020-01-01 16:28:19 +0530

    Add files via upload

---
+++ WcfAppDemo/WebAspClientAsync/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAspClientAsync.Controllers
+{
+    public class HomeController : AsyncController
+    {
+        public void IndexAsync()
+        {
+            ServiceReference1.Service1Client service = new ServiceReference1.Service1Client();
+            service.GetDataCompleted += Service_GetDataCompleted;
+            service.GetDataAsync(100);
+            AsyncManager.OutstandingOperations.Increment();
+        }
+        public ActionResult IndexCompleted()
+        {
+            return View();
+        }
+        private void Service_GetDataCompleted(object sender, ServiceReference1.GetDataCompletedEventArgs e)
+        {
+            AsyncManager.OutstandingOperations.Decrement();
+            ViewBag.Result=e.Result;
+        }
+
+        public void AboutAsync()
+        {
+            ServiceReference1.Service1Client service = new ServiceReference1.Service1Client();
+            service.HelloWelcomeCompleted += Service_HelloWelcomeCompleted;
+            service.HelloWelcomeAsync("Akshay");
+            AsyncManager.OutstandingOperations.Increment();
+            //return View();
+        }
+
+        private void Service_HelloWelcomeCompleted(object sender, ServiceReference1.HelloWelcomeCompletedEventArgs e)
+        {
+            AsyncManager.OutstandingOperations.Decrement();
+            ViewBag.Result = e.Result;
+
+        }
+
+        public ActionResult AboutCompleted()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WcfAppDemo/WebAspClientAsync/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAspClientAsync.MvcApplication" Language="C#" %>
